62c021535818359e4b0ad13d846c792bcf64354f
brotherstone97 <brotherstone97@gmail.com>
2025-03-09 18:47:52 +0900

    init project

---
+++ src/main/java/com/metlife/metsys/modernization/ModernizationApplication.java
@@ -0,0 +1,13 @@
+package com.metlife.metsys.modernization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metlife/metsys/modernization/ModernizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metlife.metsys.modernization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
